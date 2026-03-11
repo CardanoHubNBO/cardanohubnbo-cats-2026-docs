4bb544023a1f8b34bc557805ef8045883606c73b
mwihoti <danielmwihoti@gmail.com>
2026-03-11 16:58:49 +0300

    Add projects from cardano hub nbo

---
+++ content/projects/_meta.js
@@ -0,0 +1,16 @@
+export default {
+  "forkxx5pxko": "FORK",
+  "flutt3r-financekd8q12d": "Flutt3r Finance",
+  "team-dev-lordswo6w7aa": "Team Dev-lords",
+  "wanachamavlae7ky": "Wanachama",
+  "flutt3r-finance-rgqner9": "Flutt3r Finance ",
+  "hashmark-vlaeqgj": "Hashmark ",
+  "lite-certekgrnoa": "Lite Cert",
+  "power-housepdnxble": "Power House",
+  "harvest-ledgergdvld1n": "Harvest Ledger",
+  "zaoypbqxqp": "Zao",
+  "scalifyobzp9jx": "Scalify",
+  "archers81ybozr": "Archers",
+  "spikey5balj6": "Spike",
+  "flutt3r-financergqkv5l": "Flutt3r Finance"
+}
